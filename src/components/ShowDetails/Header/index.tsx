b831b7a7d73b056904e79a172f3a579ca45c5de1
mahipal-haritwal <mahipal.haritwal@qapitacorp.com>
2023-01-22 22:12:54 +0530

    chore: create header for view show page

---
+++ src/components/ShowDetails/Header/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import "./index.css";
+
+const NavigationHeader = () => {
+  const navigate = useNavigate();
+
+  function goBack() {
+    navigate("/");
+  }
+  return (
+    <div className="head">
+      <ArrowBackIcon
+        onClick={goBack}
+        accentHeight={50}
+        style={{ cursor: "pointer" }}
+      />
+      <h2> All Shows</h2>
+    </div>
+  );
+};
+
+export default NavigationHeader;
