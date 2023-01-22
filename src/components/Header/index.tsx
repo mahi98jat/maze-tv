25551f51b99fb76a5e56bd0c2a31995ff641fb35
mahipal-haritwal <mahipal.haritwal@qapitacorp.com>
2023-01-22 22:13:56 +0530

    chore: create header component for home page

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,36 @@
+import React, { FC } from "react";
+import { Input } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import { useAppStore } from "../Store";
+import "./index.css";
+
+export const Header: FC = () => {
+  const [setSearchParams] = useAppStore((state) => [state.setSearchParams]);
+
+  let id: any;
+  const handleSearch: React.ChangeEventHandler = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    if (!id) {
+      setSearchParams(e.target.value);
+      id = setTimeout(() => {
+        clearTimeout(id);
+      }, 400);
+    } else {
+    }
+  };
+
+  return (
+    <header className="header">
+      <img src="https://static.tvmaze.com/images/tvm-header-logo.png" />
+      <div>
+        <SearchIcon />
+        <Input
+          name="search"
+          placeholder="Search here"
+          onChange={handleSearch}
+        />
+      </div>
+    </header>
+  );
+};
