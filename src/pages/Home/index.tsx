3bfc5a20f02e5135fb81d062795c82a08f65c363
mahipal-haritwal <mahipal.haritwal@qapitacorp.com>
2023-01-22 22:12:18 +0530

    chore: create home page and show details page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+import Main from "../../components/Main";
+import { Header } from "../../components";
+
+const Home: FC = () => {
+  return (
+    <div>
+      <Header />
+      <Main />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/ViewShow/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { ShowDetails } from "../../components/ShowDetails";
+
+const ViewShow = () => {
+  return <ShowDetails />;
+};
+
+export default ViewShow;
